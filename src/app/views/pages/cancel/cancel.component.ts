e658530814c2be5d99911c2a2339eb8e65b212f2
Ivam Galvão Filho <ivam.developer@gmail.com>
2018-07-03 21:10:58 -0300

    Integration_api_07

--- src/app/views/pages/cancel/cancel.component.ts
+++ src/app/views/pages/cancel/cancel.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { AppService } from '../../../app.service';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-cancel',
   templateUrl: './cancel.component.html',
-  styleUrls: ['./cancel.component.sass']
+  styleUrls: ['./cancel.component.sass'],
+  providers: [AppService]
 })
 export class CancelComponent implements OnInit {
-
-  constructor() { }
+  form_cancel: FormGroup;
+  getCancel:  string;
+  arrowCancel: string[] = [''];
+  constructor(
+    private formBuilder: FormBuilder,
+    private httpAppService: AppService
+  ) { }
 
   ngOnInit() {
+    const returnCancel = this.httpAppService.getShows()
+    .subscribe(
+      data => this.getCancel = data,
+      error => alert(error)
+   );
+   this.form_cancel = this.formBuilder.group({
+
+    name: ['', [Validators.required, Validators.nullValidator]],
+    reservation_name: ['', [Validators.required, Validators.nullValidator]],
+    status_armchairs: ['', [Validators.required, Validators.nullValidator]]
+
+  });
   }
 
 }
--- src/app/views/pages/reserve/reserve.component.ts
+++ src/app/views/pages/reserve/reserve.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { AppService } from '../../../app.service';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reserve',
   templateUrl: './reserve.component.html',
-  styleUrls: ['./reserve.component.sass']
+  styleUrls: ['./reserve.component.sass'],
+  providers: [AppService]
 })
 export class ReserveComponent implements OnInit {
-
-  constructor() { }
+  form_reserve: FormGroup;
+  getReserve:  string;
+  arrowReserve: string[] = [''];
+  constructor(
+    private formBuilder: FormBuilder,
+    private httpAppService: AppService
+  ) { }
 
   ngOnInit() {
+    const returnReserve = this.httpAppService.getShows()
+    .subscribe(
+      data => this.getReserve = data,
+      error => alert(error)
+   );
+   this.form_reserve = this.formBuilder.group({
+
+    name: ['', [Validators.required, Validators.nullValidator]],
+    reservation_name: ['', [Validators.required, Validators.nullValidator]],
+    status_armchairs: ['', [Validators.required, Validators.nullValidator]]
+
+  });
   }
 
 }
